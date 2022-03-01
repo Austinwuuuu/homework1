c34b31c976ffe80cd4b75327296607e806da3351
Austinwuuuu <100336919+Austinwuuuu@users.noreply.github.com>
2022-03-01 18:13:36 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <title>helloPHP</title>
+</head>
+<body>
+    <body bgcolor="Tomato">
+    <?php
+        $Variablet1="hellophp";
+        $Number1=60;
+        echo "<font color=blue><h1>firstString字串</h2></font></br>";
+        echo "<h2>$Variablet1</h2></br>";
+        echo date("Y/m/d");
+        echo "</br>";
+        echo "$Number1";
+    ?>
+</body>
+</html>
